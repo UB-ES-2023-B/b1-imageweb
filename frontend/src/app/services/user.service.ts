ec54b9f156d81bf2540c04dbc7a78640fc4d4ff9
IceFox72 <pferreme7@alumnes.ub.edu>
2023-10-18 14:17:30 +0200

    ver perfil ended

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse   } from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { GlobalDataService } from './global-data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+              private globalDataService: GlobalDataService) { }
+
+  getUser(username: string): Observable<any> {
+    const token = this.globalDataService.getToken();
+
+    // Crea un objeto HttpHeaders con el encabezado de autorización
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    // Realiza la solicitud HTTP con los encabezados
+    return this.http.get(`/api/user/getByUserName/${username}`, { headers, observe: 'response' });
+  }
 }
