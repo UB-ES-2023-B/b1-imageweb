f90234376690485a811c1d46965ace765e705adf
Likeamarc <caminalmarc@gmail.com>
2023-11-06 13:03:25 +0100

    testing de Users

---
+++ src/test/java/com/example/b1esimageweb/UserControllerIntegrationTest.java
@@ -0,0 +1,117 @@
+package com.example.b1esimageweb;
+
+import com.example.b1esimageweb.service.UserService;
+import com.example.b1esimageweb.web.dto.UserRegistrationDto;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@EnableWebSecurity
+public class UserControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserService userService;
+
+    private static UserRegistrationDto testUserDto;
+
+    /*
+    @BeforeAll
+    @WithMockUser (username = "adminUser", password = "password", roles = "ADMIN")
+    public static void setUp() {
+        // Create a test user
+        testUserDto = new UserRegistrationDto("testing", "testuser@example.com", "123456");
+    }
+    @Test
+    @WithMockUser (username = "adminUser", password = "password", roles = "ADMIN")
+    public void testRegisterUser() throws Exception {
+        String jsonRequest = "{ \"username\": \"testUser\", \"password\": \"testPassword\", \"email\": \"test@example.com\" }";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .contentType("application/json")
+                        .content(jsonRequest))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print()); // This prints the response for debugging
+    }
+ */
+
+    @Test
+    @Order(1)
+    @WithMockUser (username = "adminUser", password = "password", roles = "ADMIN")
+    public void testGetAllUsers() throws Exception {
+        mockMvc.perform(get("/user/getAll"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+
+    @Test
+    @Order(2)
+    @WithMockUser (username = "adminUser", password = "password", roles = "ADMIN")
+    public void testGetUserById() throws Exception {
+        mockMvc.perform(get("/user/getById/{id}", 1))  // Replace 1 with an actual user ID
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @Order(3)
+    @WithMockUser (username = "adminUser", password = "password", roles = "ADMIN")
+    public void testGetUserByUserName() throws Exception {
+        mockMvc.perform(get("/user/getByUserName/{userName}", "daniTesting31"))  // Replace "username" with an actual username
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    /*
+    @Test
+    @Order(4)
+    @WithMockUser (username = "adminUser", password = "password", roles = "ADMIN")
+    public void testUpdateUser() throws Exception {
+        // Prepare a JSON request body for updating a user
+        String requestBody = "{\"username\":\"daniTesting31\",\"email\":\"newEmail@example.com\"}";
+
+        mockMvc.perform(put("/user/update/{username}", "damiTesting33")  // Replace "existingUsername" with an actual username
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+    @Test
+    public void testDeleteUser() throws Exception {
+        mockMvc.perform(delete("/user/delete/{id}", 1))  // Replace 1 with an actual user ID
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testUploadUserProfilePhoto() throws Exception {
+        // Create a sample file for profile picture upload
+        MockMultipartFile file = new MockMultipartFile("profilePhoto", "sample.jpg", MediaType.IMAGE_JPEG_VALUE, "Some Content".getBytes());
+
+        mockMvc.perform(multipart("/user/uploadPhotoProfile")
+                        .file(file)
+                        .contentType(MediaType.MULTIPART_FORM_DATA))
+                .andExpect(status().isOk());
+    }
+
+ */
+}
