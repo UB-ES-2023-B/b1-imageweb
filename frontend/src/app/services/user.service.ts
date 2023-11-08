74a8a3f2a6d57cf97bcb58b5cda58f0876f44845
IceFox72 <pferreme7@alumnes.ub.edu>
2023-11-08 18:53:14 +0100

    canvis domain

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -20,16 +20,16 @@ export class UserService {
   setUserProfilePic(photo_file: File): Observable<string> {
     const formData = new FormData();
     formData.append('profilePhoto', photo_file);
-    return this.http.post('/api/user/uploadPhotoProfile', formData,{ responseType: 'text' });
+    return this.http.post(this.domain + '/user/uploadPhotoProfile', formData,{ responseType: 'text' });
   }
 
   updateUser(username: string, updatedUser: any): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     const options = { headers: headers };
-    return this.http.put(`/api/user/update/${username}`, updatedUser, options);
+    return this.http.put(this.domain + `/user/update/${username}`, updatedUser, options);
   }
 
   deleteUserProfilePhoto(): Observable<string> {
-    return this.http.delete('/api/user/deleteProfilePhoto', { responseType: 'text' });
+    return this.http.delete(this.domain + '/user/deleteProfilePhoto', { responseType: 'text' });
   }
 }
