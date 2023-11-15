51c3ac9737ec4fdd5f3c6eecfa1fcdab259adde0
IceFox72 <pferreme7@alumnes.ub.edu>
2023-11-15 18:22:25 +0100

    errors contrasenya

--- frontend/src/app/components/change-password/change-password.component.ts
+++ frontend/src/app/components/change-password/change-password.component.ts
@@ -10,6 +10,8 @@ export class ChangePasswordComponent {
   currentPassword: string = '';
   newPassword: string = '';
   confirmPassword: string = '';
+  showPasswordError: boolean = false;
+  showConPasswordError: boolean = false;
 
   @Output() passwordChanged = new EventEmitter<any>();
   @Output() modalClosed = new EventEmitter<void>();
@@ -17,13 +19,41 @@ export class ChangePasswordComponent {
   constructor() {}
 
   changePassword(): void {
-    // Lógica para cambiar la contraseña
-    this.passwordChanged.emit({
-      currentPassword: this.currentPassword,
-      newPassword: this.newPassword,
-      confirmPassword: this.confirmPassword
-    });
-    // Cierra el modal después de cambiar la contraseña
-    this.modalClosed.emit();
+    if (this.showPasswordError && this.showConPasswordError) {
+      // ERROR
+    }
+    else {
+      // COMPARAR CONTRASENYES ANTIGUES
+      // SPINNER DE CARREGANT
+      // CANVIAR DADES A BD
+      // CONFIRMACIÓ
+      // Lógica para cambiar la contraseña
+      this.passwordChanged.emit({
+        currentPassword: this.currentPassword,
+        newPassword: this.newPassword,
+        confirmPassword: this.confirmPassword
+      });
+      // Cierra el modal después de cambiar la contraseña
+      this.modalClosed.emit();
+    }
+  }
+
+  onPasswordChange() {
+    this.showPasswordError = false;
+  }
+
+  onPasswordBlur() {
+    const passRegex = /^(?=.*[!@#$%^&*,.])((?=.*[A-Z])(?=.*[a-z])(?=.*\d)).{6,}$/;
+    if (this.newPassword.length > 0) this.showPasswordError = !passRegex.test(this.newPassword);
+  }
+
+  onConPasswordChange() {
+    this.showConPasswordError = false;  // Reset error message on password change
+  }
+
+  onConPasswordBlur() {
+    if(this.newPassword != this.confirmPassword){
+      this.showConPasswordError = true;
+    }
   }
 }
