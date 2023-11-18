62876d94fec39a780b32d8ecc7192184a424dbd5
danielaocampo2 <danielaocampoo@gmail.com>
2023-11-18 15:51:50 +0100

    edit gallery fields component almost finished

---
+++ frontend/src/app/components/edit-gallery/edit-gallery.component.ts
@@ -0,0 +1,156 @@
+import { Component } from '@angular/core';
+import { GalleryService } from 'src/app/services/gallery.service';
+import { GlobalDataService } from 'src/app/services/global-data.service';
+import { Subscription } from 'rxjs';
+import { Lightbox } from 'ngx-lightbox';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-edit-gallery',
+  templateUrl: './edit-gallery.component.html',
+  styleUrls: ['./edit-gallery.component.css']
+})
+export class EditGalleryComponent {
+  loading:boolean=true;
+  images:any[]=[];
+  showUploadHint: boolean = true;
+  isEditMode = false;
+  selectedImageIds: number[] = [];
+  editImageId:number=-1;
+
+  modalResponse: string = '';
+
+  classModal:string='text-center mb-4'
+
+
+  private imagesSubscription: Subscription = new Subscription();
+
+
+  constructor(private galleryService:GalleryService, private router:Router, private globalDataService:GlobalDataService, private _lightbox: Lightbox, private toastr: ToastrService){
+  }
+  goGallery(){
+    this.router.navigate(['/profile/']);
+  }
+
+  changeIsModal(){
+    this.classModal='text-center mb-4 hideText'
+  }
+
+  handleModalClosed(response: string) {
+
+
+    if(response=='Save click'){
+      this.loading=true;
+      this.deleteSelectedImages()
+    }else{
+      this.classModal='text-center mb-4'
+    }
+
+    this.modalResponse = response;
+
+  }
+
+  startEditInfo(id:number){
+    this.editImageId=id;
+  }
+  cancelEditInfo(){
+    this.editImageId=-1;
+  }
+  saveEditInfo(id:number, name:string, description:string){
+    console.log('id foto::', id, 'este es el name:', name,'este es el description:: ', description )
+    //Hacer la llamada al back
+    //this.editImageId=-1;
+  }
+  getGallery():void{
+    this.images=[]
+    this.galleryService.getGalleryUser(this.globalDataService.getUsername()).subscribe(
+      (response)=>{
+        if (response.body && Array.isArray(response.body)) {
+          response.body.forEach((element: any) => {
+            if (element.data) {
+              this.images.unshift({
+            "src":`data:image/${element.photoExtensio};base64,${element.data}`,
+             "id": element.photoId, "name":element.photoName, "description": 'Cambiar descripción'});
+            }
+          });
+        }
+        this.galleryService.setImages(this.images);
+        this.loading=false;
+
+      },
+      (error)=>{
+        console.log('error al obtener all gallery', error)
+      }
+    )
+  }
+  ngOnInit(): void {
+    this.imagesSubscription = this.galleryService.getImagesObservable().subscribe((images) => {
+      this.images = images;
+    });
+
+    this.getGallery();
+
+  }
+
+
+  open(index: number): void {
+    // open lightbox
+    this._lightbox.open(this.images, index);
+  }
+
+  close(): void {
+    // close lightbox programmatically
+    this._lightbox.close();
+  }
+  ngOnDestroy(): void {
+    this.imagesSubscription.unsubscribe();
+  }
+
+
+
+  cancelEditMode() {
+    this.isEditMode = false;
+    this.selectedImageIds = [];
+  }
+
+
+  deleteSelectedImages() {
+
+    this.galleryService.deletePhotoGallery( this.selectedImageIds).subscribe(
+      (response)=>{
+        console.log('Se ha borrado bien', response)
+        this.getGallery()
+      },
+      (error)=>{
+        console.log('error al eliminar', error)
+        this.toastr.error('No se ha eliminado correctamente','Error');
+
+      }
+    )
+
+    this.isEditMode = false;
+    this.classModal='text-center mb-4 '
+    this.selectedImageIds = [];
+  }
+  toggleEditMode(id: number) {
+
+    if (this.isEditMode) {
+      // Si ya estamos en modo de edición, significa que se hizo clic en el checkbox
+      const index = this.selectedImageIds.indexOf(id);
+      if (index === -1) {
+        // Si el ID no está en la lista, lo añadimos
+        this.selectedImageIds.push(id);
+      } else {
+        // Si el ID ya está en la lista, lo eliminamos
+        this.selectedImageIds.splice(index, 1);
+      }
+    } else {
+      // Si no estamos en modo de edición, activamos el modo y añadimos la imagen actual
+      this.isEditMode = true;
+      this.selectedImageIds.push(id);
+    }
+    // console.log('Lista de imágenes seleccionadas:', this.selectedImageIds);
+  }
+
+
+}
--- frontend/src/app/components/gallery/gallery.component.ts
+++ frontend/src/app/components/gallery/gallery.component.ts
@@ -4,6 +4,7 @@ import { GlobalDataService } from 'src/app/services/global-data.service';
 import { Subscription } from 'rxjs';
 import { Lightbox } from 'ngx-lightbox';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -17,6 +18,7 @@ export class GalleryComponent {
   showUploadHint: boolean = true;
   isEditMode = false;
   selectedImageIds: number[] = [];
+  editImageId:number=-1;
 
   modalResponse: string = '';
 
@@ -26,27 +28,13 @@ export class GalleryComponent {
   private imagesSubscription: Subscription = new Subscription();
 
 
-  constructor(private galleryService:GalleryService, private globalDataService:GlobalDataService, private _lightbox: Lightbox, private toastr: ToastrService){
+  constructor(private galleryService:GalleryService, private router: Router, private globalDataService:GlobalDataService, private _lightbox: Lightbox, private toastr: ToastrService){
   }
 
 
-  changeIsModal(){
-    this.classModal='text-center mb-4 hideText'
+  goEditGallery(){
+    this.router.navigate(['/profile/editGallery']);
   }
-
-  handleModalClosed(response: string) {
-
-
-    if(response=='Save click'){
-      this.deleteSelectedImages()
-    }else{
-      this.classModal='text-center mb-4'
-    }
-
-    this.modalResponse = response;
-
-  }
-
 
   getGallery():void{
     this.images=[]
@@ -92,55 +80,6 @@ export class GalleryComponent {
   ngOnDestroy(): void {
     this.imagesSubscription.unsubscribe();
   }
-
-
-
-  cancelEditMode() {
-    this.isEditMode = false;
-    this.selectedImageIds = [];
-  }
-
-
-  deleteSelectedImages() {
-
-    this.galleryService.deletePhotoGallery( this.selectedImageIds).subscribe(
-      (response)=>{
-        console.log('Se ha borrado bien', response)
-        this.loading=true;
-        this.getGallery()
-      },
-      (error)=>{
-        console.log('error al eliminar', error)
-        this.toastr.error('No se ha eliminado correctamente','Error');
-
-      }
-    )
-
-    this.isEditMode = false;
-    this.classModal='text-center mb-4 '
-    this.selectedImageIds = [];
-  }
-  toggleEditMode(id: number) {
-
-    if (this.isEditMode) {
-      // Si ya estamos en modo de edición, significa que se hizo clic en el checkbox
-      const index = this.selectedImageIds.indexOf(id);
-      if (index === -1) {
-        // Si el ID no está en la lista, lo añadimos
-        this.selectedImageIds.push(id);
-      } else {
-        // Si el ID ya está en la lista, lo eliminamos
-        this.selectedImageIds.splice(index, 1);
-      }
-    } else {
-      // Si no estamos en modo de edición, activamos el modo y añadimos la imagen actual
-      this.isEditMode = true;
-      this.selectedImageIds.push(id);
-    }
-    // console.log('Lista de imágenes seleccionadas:', this.selectedImageIds);
-  }
-
-
 
 
 
