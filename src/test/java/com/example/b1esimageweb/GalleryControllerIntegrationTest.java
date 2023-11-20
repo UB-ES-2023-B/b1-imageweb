4f8c867cde3beacbc50a926f561e8466c5ad9918
Likeamarc <caminalmarc@gmail.com>
2023-11-20 17:46:25 +0100

    galleryIntegrationTest terminado

--- src/test/java/com/example/b1esimageweb/GalleryControllerIntegrationTest.java
+++ src/test/java/com/example/b1esimageweb/GalleryControllerIntegrationTest.java
@@ -6,7 +6,9 @@ import com.example.b1esimageweb.repository.UserRepository;
 import com.example.b1esimageweb.service.GalleryService;
 import com.example.b1esimageweb.service.UserService;
 import com.example.b1esimageweb.web.Jwt.JwtTokenProvider;
+import com.example.b1esimageweb.web.dto.PhotoDto;
 import com.example.b1esimageweb.web.dto.UserRegistrationDto;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +27,9 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -52,42 +57,82 @@ public class GalleryControllerIntegrationTest {
 
 
     @BeforeAll
-    @WithMockUser (username = "adminUser", password = "password", roles = "ADMIN")
+    @WithMockUser (username = "adminUser", password = "admin", roles = "ADMIN")
     public void setUp() {
         // Create a test user
-        User user=userRepository.findByUsername("adminUser").orElseThrow();
+        User user=userRepository.findByUsername("testing23Marc").orElseThrow();
         userToken = tokenProvider.createToken(user);
     }
 
     @Test
     @Order(1)
-    @WithMockUser (username = "adminUser", password = "password", roles = "ADMIN")
+    @WithMockUser (username = "testing23Marc", password = "1234Asd.")
     public void testUserGallery() throws Exception {
-        MvcResult result = mockMvc.perform(get("/gallery/viewGalleryFromUser/{userName}", "adminUser")
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + userToken))
+        /*
+        String jsonRequest = "{ \"username\": \"testing23Marc\", \"password\": \"1234Asd.\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth/login")
+                        .contentType("application/json")
+                        .content(jsonRequest))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+        */
+        MvcResult result = mockMvc.perform(get("/user/getByUserName/{userName}", "testing23Marc"))  // Replace "username" with an actual username
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andReturn();
-        /*
-        String jsonGallery = result.getResponse().getContentAsString();
-        System.out.println(jsonGallery);
-        Gallery gallery = new ObjectMapper().readValue(jsonGallery, Gallery.class);
 
-        int id = gallery.getGalleryId();
+        String jsonUser = result.getResponse().getContentAsString();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(jsonUser);
+        JsonNode test = jsonNode.get("gallery");
+        int id = test.get("galleryrId").asInt();
+        System.out.println("Gallery ID" + id);
 
         mockMvc.perform(get("/gallery/viewGallery/{galleryId}", id)
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + userToken))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
 
-        MockMultipartFile file = new MockMultipartFile("testPhoto", "../../images/sample.jpg", MediaType.IMAGE_JPEG_VALUE, "Some Content".getBytes());
+        MockMultipartFile file = new MockMultipartFile("photo", "../../images/sample.jpg", MediaType.IMAGE_JPEG_VALUE, "Some Content".getBytes());
 
-        mockMvc.perform(multipart("/uploadPhotoGalery/{galleryId}", id)
+        
+        mockMvc.perform(multipart("/gallery/uploadPhotoGalery/{galleryId}", id)
                         .file(file)
                         .contentType(MediaType.MULTIPART_FORM_DATA)
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + userToken))
                 .andExpect(status().isOk());
-        */
+
+
+        MvcResult jsonPhotos = mockMvc.perform(get("/gallery/viewGalleryFromUser/{userName}", "testing23Marc")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + userToken))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        String jsonGallery = jsonPhotos.getResponse().getContentAsString();
+        System.out.println(jsonGallery);
+        ObjectMapper objectMapper2 = new ObjectMapper();
+        JsonNode jsonNode2 = objectMapper2.readTree(jsonGallery);
+        JsonNode lastObject = jsonNode2.get(jsonNode2.size() - 1);
+        int photoId = lastObject.get("photoId").asInt();
+        List<Integer> photoIds = new ArrayList<>();
+        photoIds.add(photoId);
+        System.out.println(photoIds);
+
+        JsonNode jsonNodeDelete = objectMapper.createObjectNode()
+                .putPOJO("photoIds", photoIds);
+
+        String photoIdsJson = objectMapper.writeValueAsString(jsonNodeDelete);
+
+
+        mockMvc.perform(delete("/gallery/deletephotos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(photoIdsJson)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + userToken))
+                .andExpect(status().isOk());
+
+
     }
 
 
