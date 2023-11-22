1d8d6ae9b082c1934c9254821682aa8f8eb59f31
oscarmusol <oscar.musol@gmail.com>
2023-11-22 14:13:58 +0100

    Albums first commit

---
+++ frontend/src/app/components/album-view/album-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-album-view',
+  templateUrl: './album-view.component.html',
+  styleUrls: ['./album-view.component.css']
+})
+export class AlbumViewComponent {
+
+}
