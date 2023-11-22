030f8c963125b718133f760069d30d1bb062a472
danielaocampo2 <danielaocampoo@gmail.com>
2023-11-22 01:36:25 +0100

    connect back but errors

--- frontend/src/app/services/gallery.service.ts
+++ frontend/src/app/services/gallery.service.ts
@@ -41,6 +41,15 @@ export class GalleryService {
     });
   }
 
+  editInfoPhoto(idPhoto:number, name:string, description:string): Observable<any>{
+    const formData = new FormData();
+    // console.log('mira el photo file', photo_file)
+    formData.append('photoName', name);
+    formData.append('photoDescription', description);
+
+    return this.http.put(this.domain + `/gallery/editInfoPhoto/${idPhoto}`, formData,{  observe: 'response'});
+  }
+
   setImages(images: any[]): void {
     this.images = images;
     this.imagesSubject.next(this.images);
