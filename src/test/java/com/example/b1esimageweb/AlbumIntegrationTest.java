fd7372608bc04918891b71e3d72d44d9ac560f80
Likeamarc <caminalmarc@gmail.com>
2023-12-01 22:05:05 +0100

    cambios en el sistema de testing

--- src/test/java/com/example/b1esimageweb/AlbumIntegrationTest.java
+++ src/test/java/com/example/b1esimageweb/AlbumIntegrationTest.java
@@ -1,4 +1,53 @@
 package com.example.b1esimageweb;
 
+import com.example.b1esimageweb.model.User;
+import com.example.b1esimageweb.repository.UserRepository;
+import com.example.b1esimageweb.web.Jwt.JwtTokenProvider;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@EnableWebSecurity
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AlbumIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+    @Autowired
+    private UserRepository userRepository;
+    private String userToken;
+
+
+    @BeforeAll
+    public void setUp() {
+        // Create a test user
+        User user=userRepository.findByUsername("testing23Marc").orElseThrow();
+        userToken = tokenProvider.createToken(user);
+    }
+
+    @Test
+    @Order(1)
+    public void testGetAlbum() throws Exception {
+        mockMvc.perform(get("/getAlbum/{albumId}", 1)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + userToken))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
 }
